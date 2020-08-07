c5964989a153a93b4e9795b76bb3ac73cb6b10ec
dhaydt <1992dayat@gmail.com>
2020-08-07 18:15:19 +0700

    second commit

---
+++ build/precache-manifest.f49804e3bffef7356ce7792222b13105.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f93cda92245eb832a9ec98c23168bf9",
+    "url": "/trueinsight/index.html"
+  },
+  {
+    "revision": "8728eab390d49f75e063",
+    "url": "/trueinsight/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "a57e34c6b450701358f0",
+    "url": "/trueinsight/static/css/main.7c0ff9fd.chunk.css"
+  },
+  {
+    "revision": "8728eab390d49f75e063",
+    "url": "/trueinsight/static/js/2.7e3d0687.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/trueinsight/static/js/2.7e3d0687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a57e34c6b450701358f0",
+    "url": "/trueinsight/static/js/main.b5127ca5.chunk.js"
+  },
+  {
+    "revision": "f875901bd04dc65622c7",
+    "url": "/trueinsight/static/js/runtime-main.eeecf894.js"
+  },
+  {
+    "revision": "e4093059547b462e2136e7c496a533c6",
+    "url": "/trueinsight/static/media/appdt.e4093059.png"
+  },
+  {
+    "revision": "6dab99b67d4b0326df6de784959c62e3",
+    "url": "/trueinsight/static/media/appmb.6dab99b6.png"
+  },
+  {
+    "revision": "9c8ee4f8d1efcb2e3b26b77663727fe3",
+    "url": "/trueinsight/static/media/design.9c8ee4f8.png"
+  },
+  {
+    "revision": "3514e38783e121c388bf0e3ba50e8d9a",
+    "url": "/trueinsight/static/media/ecmr.3514e387.png"
+  },
+  {
+    "revision": "90cddf3f9f310149b723400bc5089c21",
+    "url": "/trueinsight/static/media/game.90cddf3f.png"
+  },
+  {
+    "revision": "2002c61e5e770bdcd5e132757deabe73",
+    "url": "/trueinsight/static/media/img.2002c61e.svg"
+  },
+  {
+    "revision": "24c36218b2fef8ca438cad39c79f4f13",
+    "url": "/trueinsight/static/media/img2.24c36218.svg"
+  },
+  {
+    "revision": "3372f60935dff6d4162f8d3c22b0b539",
+    "url": "/trueinsight/static/media/s1.3372f609.jpg"
+  }
+]);
